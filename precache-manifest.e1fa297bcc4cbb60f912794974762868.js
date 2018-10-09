bce9394dab77d2b8dc4223414bd96a6f07e81e1c
angww <angelo18@gmail.com>
2018-10-08 22:50:04 -0300

    Updates

---
+++ precache-manifest.e1fa297bcc4cbb60f912794974762868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9fd193f3c90da4d93e6",
+    "url": "/TabelaTurno/static/js/runtime~main.f9fd193f.js"
+  },
+  {
+    "revision": "f437db5ff3483f599dab",
+    "url": "/TabelaTurno/static/js/main.f437db5f.chunk.js"
+  },
+  {
+    "revision": "5885287c85f35d160b50",
+    "url": "/TabelaTurno/static/js/1.5885287c.chunk.js"
+  },
+  {
+    "revision": "f437db5ff3483f599dab",
+    "url": "/TabelaTurno/static/css/main.db709cb3.chunk.css"
+  },
+  {
+    "revision": "5fa950cae50a2d2e42cca402404a1838",
+    "url": "/TabelaTurno/index.html"
+  }
+];
