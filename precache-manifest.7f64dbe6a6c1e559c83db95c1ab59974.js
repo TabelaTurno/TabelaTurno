5d8e300a773df8a17cc34ead6d28fb46d679d3e8
angww <angelo18@gmail.com>
2018-11-26 10:22:50 -0200

    Updates

--- precache-manifest.7f64dbe6a6c1e559c83db95c1ab59974.js
+++ precache-manifest.7f64dbe6a6c1e559c83db95c1ab59974.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/TabelaTurno/static/js/runtime~main.f9fd193f.js"
   },
   {
-    "revision": "8112d9ab54255160e004",
-    "url": "/TabelaTurno/static/js/main.8112d9ab.chunk.js"
+    "revision": "8940f03689e06f0fc91e",
+    "url": "/TabelaTurno/static/js/main.8940f036.chunk.js"
   },
   {
     "revision": "9a24882bc93c8cbabbb1",
     "url": "/TabelaTurno/static/js/1.9a24882b.chunk.js"
   },
   {
-    "revision": "8112d9ab54255160e004",
+    "revision": "8940f03689e06f0fc91e",
     "url": "/TabelaTurno/static/css/main.ec406eba.chunk.css"
   },
   {
-    "revision": "61208c1cc5321b7b19756018a87e6e76",
+    "revision": "bbb7a024ca83141a20a472fffe3ef2ce",
     "url": "/TabelaTurno/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/TabelaTurno/precache-manifest.ac3c6143bd929a9d5a15819800826a66.js"
+  "/TabelaTurno/precache-manifest.7f64dbe6a6c1e559c83db95c1ab59974.js"
 );
 
 workbox.clientsClaim();
