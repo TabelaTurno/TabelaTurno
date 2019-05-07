a8b21e4ff12f7530d3b8de3fb7229e97d3f08346
angww <angelo18@gmail.com>
2019-05-07 00:41:51 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/TabelaTurnoRefap/precache-manifest.a1864aab596b76a23bdde58454623d0d.js"
+  "/TabelaTurno/precache-manifest.b24521adc441d0063514ac6a0b08e158.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TabelaTurnoRefap/index.html", {
+workbox.routing.registerNavigationRoute("/TabelaTurno/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
