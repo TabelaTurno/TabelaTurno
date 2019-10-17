0f1f689f678848c758929c04c8182540e75580dd
Angelo Martins <angelo18@gmail.com>
2019-10-17 00:39:56 -0300

    Updates

---
+++ precache-manifest.a496571bbdd5eef39673ae16fd43e8a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633b39e192a74231f312674ee45e2b64",
+    "url": "/TabelaTurno/index.html"
+  },
+  {
+    "revision": "c80cdcd5952267384ef2",
+    "url": "/TabelaTurno/static/css/main.156e5a3d.chunk.css"
+  },
+  {
+    "revision": "45ede8384a7b29d74976",
+    "url": "/TabelaTurno/static/js/2.5473133a.chunk.js"
+  },
+  {
+    "revision": "c80cdcd5952267384ef2",
+    "url": "/TabelaTurno/static/js/main.12e7fe1e.chunk.js"
+  },
+  {
+    "revision": "cedbfd86d9d538397727",
+    "url": "/TabelaTurno/static/js/runtime-main.1a29d035.js"
+  }
+]);
