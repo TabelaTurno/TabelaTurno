539a57c09036cf87d998037759ccad4b9f7b3103
Angelo Araujo <angelo18@gmail.com>
2021-09-22 22:18:41 -0300

    Fix alert message of new table at Refap.

--- src/tabela2.js
+++ src/tabela2.js
@@ -184,24 +184,27 @@ class Tabela2 extends React.Component {
         
         let extTick = this.tick.bind(this); // To use method in next line
         extTick(); // Load next month as DidMount.
-        
-        // eslint-disable-next-line
-        let timer2 = setTimeout(function () { // Wait 80ms and alert user
-            let alert_table = this.state.tableInput.localeCompare("Refap0") === 0;
-            if (alert_table) {
-                alert("Aguardando definição oficial da empresa para atualizar tabela.");
-                trackEvent('UserAlert', 'AlertToscoEsperandoTabelaOficial', 'label4'); 
-            }
-        }, 80);
 
     }
 
     
     componentDidUpdate(prevProps) {
         if (this.props.tableName !== prevProps.tableName) {
+            
+            // Alert on other tables.
+            if (this.props.tableName.localeCompare("Refap12x12")) {
+                // eslint-disable-next-line
+                let timer2 = setTimeout(function () { // Wait 80ms and alert user
+                    alert("Aguardando definição oficial da empresa para atualizar tabela.");
+                    trackEvent('UserAlert', 'AlertToscoEsperandoTabelaOficial', 'label4'); 
+                
+                }, 80);
+            }
           this.initTableComponent();
           let extTick = this.tick.bind(this);
           extTick();
+        
+
         }
       }
 
